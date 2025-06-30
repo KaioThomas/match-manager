3e98722b606bd00ae5dac55163cfbc0cd82e3605
Kaio Thomas Pereira Souza <kaio.psouza+meli@mercadolivre.com>
2025-06-30 16:15:09 -0300

    :sparkles: feat: exceptions

---
+++ src/main/java/br/com/meli/soccer/match_manager/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.meli.soccer.match_manager.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
